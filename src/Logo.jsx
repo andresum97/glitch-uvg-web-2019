14b0616b047d2097f6473ecf29e1925a52932984
Andres Urizar <uri17632@uvg.edu.gt>
2019-10-02 23:31:36 -0600

    Correcciones realizadas
    Correciones realizadas en el codigo, utilizando ESLint

---
+++ src/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+const Logo = () => (
+  <div clasName="Container">
+    <img src="https://duckduckgo.com/assets/logo_homepage.normal.v108.svg" href={window.scrollTo(0, 250)} className="mainLogo" alt="" />
+
+    <span className="tag"> `&quot;`conocer más`&quot;`</span>
+  </div>
+);
+
+
+export default Logo();
---
+++ src/button-dist/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+const DiscButton = () => (
+  <div className="install">
+    <button type="button">Add DuckDuckGo to Firefox</button>
+  </div>
+);
+
+
+export default DiscButton();
